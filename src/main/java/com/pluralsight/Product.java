c8e8f3cb0bc009f3f732a3ed006a94df2c43e2d0
Nauman <naumansqb@gmail.com>
2025-10-18 12:56:25 -0400

    Product.java - Setup attributes, getters/setters, toString()

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
 public class Product {
+    private String id;
+    private String description;
+    private double price;
+
+    public Product(double price, String id, String description) {
+        this.price = price;
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
